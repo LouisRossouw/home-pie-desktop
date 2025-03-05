d5a27133373329d962e8e89261ea71f86583ff72
Louis <lourou99@gmail.com>
2025-03-05 17:35:39 +0200

    React router

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,11 +1,11 @@
-function App(): JSX.Element {
+import { AppRoutes } from './app-routes'
+
+export default function App(): JSX.Element {
   const ipcHandle = (): void => window.electron.ipcRenderer.send('ping')
 
   return (
-    <div className="flex w-full h-full">
-      <p>Test</p>
+    <div className="flex items-center justify-center bg-slate-500 w-screen h-screen">
+      <AppRoutes />
     </div>
   )
 }
-
-export default App
--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -2,10 +2,13 @@ import './assets/main.css'
 
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router'
 import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
+  // <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>
+  </BrowserRouter>
+  // </React.StrictMode>
 )
