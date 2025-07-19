8636c09e1412787ffb82fb5db8c4af6f9ec6b673
Louis <lourou99@gmail.com>
2025-07-19 18:44:09 +0200

    Test theme selector

--- src/renderer/src/components/debug/index.tsx
+++ src/renderer/src/components/debug/index.tsx
@@ -6,6 +6,7 @@ import { getOAuthClients } from '@shared/auth'
 
 import { Button } from '~/components/ui/button'
 import { ApiTest } from '@shared/types'
+import { ThemeSelector } from '../theme-selector'
 
 const MODE = import.meta.env.MODE
 const isDev = import.meta.env.DEV
@@ -85,6 +86,7 @@ export function Debug() {
         {/* Test buttons go here ! */}
         <div className="flex h-full w-full border rounded-lg justify-center items-center">
           <Button onClick={() => sendTestPing()}>{isPendingPingTest ? '..' : 'Test ping'}</Button>
+          <ThemeSelector />
         </div>
       </div>
     </div>
