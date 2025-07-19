b4c7fe3b72319bb45552d26306807912b7b752ea
Louis <lourou99@gmail.com>
2025-07-19 23:57:10 +0200

    Added to app context

--- src/renderer/src/libs/context/app.tsx
+++ src/renderer/src/libs/context/app.tsx
@@ -1,23 +1,38 @@
 import { createContext, PropsWithChildren, useContext, useState } from 'react'
 
+import { Frame } from '~/libs/dot-squad'
+import { useDotSquad } from '~/libs/hooks/use-dot-squad'
+import { defaultDotSquadColour } from '~/libs/dot-squad/constants'
+
 type AppContextType = {
   isAuth: boolean
   setIsAuth: (v: boolean) => void
+  dotA: string
+  dotB: string
+  dotC: string
+  handleUpdateDotSquad: (v: Frame[]) => void
 }
 
 export const AppContext = createContext<AppContextType>({
   isAuth: false,
-  setIsAuth: () => {}
+  setIsAuth: () => {},
+  dotA: defaultDotSquadColour,
+  dotB: defaultDotSquadColour,
+  dotC: defaultDotSquadColour,
+  handleUpdateDotSquad: () => {}
 })
 
 export const AppContextProvider = ({ children }: PropsWithChildren) => {
+  const dotSquad = useDotSquad()
+
   const [isAuth, setIsAuth] = useState(false)
 
   return (
     <AppContext.Provider
       value={{
         isAuth,
-        setIsAuth
+        setIsAuth,
+        ...dotSquad
       }}
     >
       {children}
