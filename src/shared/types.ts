3b45f887ea39fcb0f1fd6a6f83d6f19e971ef3f0
Louis <lourou99@gmail.com>
2025-07-21 15:19:29 +0200

    Added settings type

--- src/shared/types.ts
+++ src/shared/types.ts
@@ -1,5 +1,7 @@
 // TODO; Maybe switch to open-api, to generate api response types from my server.
 
+import { defaultAppSettings } from '@main/src/default-app-settings'
+
 export type ApiTest = { ok: boolean; data?: any }
 
 export type Project = {
@@ -10,3 +12,5 @@ export type Project = {
 }
 
 export type ApiProjectsList = { ok: boolean; data?: Project[] }
+
+export type Setting = (typeof defaultAppSettings)[number]['slug']
