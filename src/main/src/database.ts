32a02de7030dcccfd6b38396fcab7cd2e40f24a7
Louis <lourou99@gmail.com>
2025-07-21 10:36:49 +0200

    False positive issue fix

--- src/main/src/database.ts
+++ src/main/src/database.ts
@@ -4,14 +4,16 @@ import Database from 'better-sqlite3'
 
 const fs = require('fs')
 import { SQL } from './sql'
+import { SettingSlug } from './default-app-settings'
 
 const userPath = app.getPath('userData')
 
 const dbName = 'homepie.db'
 const dbPath = path.join(userPath, dbName)
 
+export const dbExists = fs.existsSync(dbPath) // * Dont move this: Check if the DB file exists **before** initializing the database
+
 const db = new Database(dbPath)
-export const dbExists = fs.existsSync(dbPath)
 
 db.pragma('journal_mode = WAL') // it is generally important to set the WAL pragma for performance reasons. https://github.com/WiseLibs/better-sqlite3/blob/master/docs/performance.md
 
@@ -24,7 +26,7 @@ function initDatabase() {
   db.prepare(SQL.initDatabaseSQL).run()
 }
 
-function getSetting(key: string): string | undefined {
+async function getSetting(key: SettingSlug): Promise<string | undefined> {
   return db.prepare(SQL.getSettingSQL).get(key)?.value ?? undefined
 }
 
