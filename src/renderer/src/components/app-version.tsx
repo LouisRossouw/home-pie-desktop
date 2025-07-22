d551bc98f5daaea272a0fbbcfec467df9dfb36bf
Louis <lourou99@gmail.com>
2025-07-22 21:33:34 +0200

    Version semantics comment as a reminder

--- src/renderer/src/components/app-version.tsx
+++ src/renderer/src/components/app-version.tsx
@@ -1,5 +1,8 @@
 import { cn } from '~/libs/utils/cn'
 
 export function AppVersion({ className }: { className?: string }) {
-  return <p className={cn('text-light text-xs', className)}>v0.0.0 (Alpha)</p>
+  return (
+    // {/* VERSION SEMANTICS; V - MAJOR - MINOR - PATCH */}
+    <p className={cn('text-light text-xs', className)}>v0.0.0 (Alpha)</p>
+  )
 }
