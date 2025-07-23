4559482d1f198557695847e7eb3bd2413cf39021
Louis <lourou99@gmail.com>
2025-07-23 20:36:33 +0200

    App settings hook updates; retrieving and setting core app settings, wip

--- src/renderer/src/libs/hooks/use-app-settings.ts
+++ src/renderer/src/libs/hooks/use-app-settings.ts
@@ -1,39 +1,64 @@
 import { useState } from 'react'
+
+import { Setting } from '@shared/types'
 import { arrayToObject } from '~/libs/utils/utils'
 
-type AppSetting = Record<string, string | boolean | number> | undefined
+type Value = string | boolean | number
+export type AppSetting = Record<Setting, Value> | undefined
 
 export type UseAppSettings = {
-  appSettings: AppSetting
+  appSettings: AppSetting | undefined
+  getAppSetting: (v: Setting) => Promise<Value>
   getAllAppSettings: () => Promise<AppSetting>
   setAppSettings: React.Dispatch<React.SetStateAction<AppSetting>>
+  updateAppSettings: (v: { setting: Setting; value: Value }[]) => Promise<boolean>
 }
 
 export function useAppSettings() {
   const [appSettings, setAppSettings] = useState<AppSetting>(undefined)
 
-  function getAppSetting() {
-    // TODO; Filter for setting from the useState.
+  async function getAppSetting(setting: Setting) {
+    if (appSettings) return appSettings[setting]
+
+    return JSON.parse(await window.api.getAppSetting({ setting }))
   }
 
-  function updateAppSetting() {
-    // TODO; update a specific setting in useState and the db settings table.
+  async function updateAppSettings(settings: { setting: Setting; value: Value }[]) {
+    if (appSettings) {
+      settings.forEach(async (s) => {
+        appSettings[s.setting] = s.value
+        await window.api.setAppSetting({ setting: s.setting, value: JSON.stringify(s.value) })
+      })
+      return true
+    }
+
+    console.error('appSettings is undefined!')
+    return false
   }
 
   async function getAllAppSettings() {
+    if (appSettings) return appSettings
+
     const settingsArray = await window.api.getAllAppSettings()
-    const settingsObj = arrayToObject(settingsArray)
+    const settingsObj = arrayToObject(settingsArray) as AppSetting
+
     setAppSettings(settingsObj)
     return settingsObj
   }
 
-  function deleteAppSetting() {
-    // TODO; Remove a specific key from useState and the db settings table.
-  }
+  // TODO; Do i need this?
+  // async function deleteAppSetting({ setting }: { setting: Setting }) {
+  // TODO; Remove a specific key from useState and the db settings table.
+  // if(appSettings){
+  //   appSettings.dateFormat = ""
+  // }
+  // }
 
   return {
     appSettings,
+    getAppSetting,
     setAppSettings,
-    getAllAppSettings
+    getAllAppSettings,
+    updateAppSettings
   }
 }
