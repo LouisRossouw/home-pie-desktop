f3e4d5915fac17caa1d2a624c4886d2cba19756f
Louis <lourou99@gmail.com>
2025-07-23 22:48:58 +0200

    Added types

--- src/preload/api/index.ts
+++ src/preload/api/index.ts
@@ -32,7 +32,7 @@ const externalAPI = {
 const databaseAppSettingsAPI = {
   test: (data: { v: boolean }) => IPCR.invoke('test', data),
   getAppSetting: async (data: { setting: Setting }) => IPCR.invoke('get-app-setting', data),
-  setAppSetting: async (data: { setting: Setting; value: string }) => IPCR.invoke('set-app-setting', data),
+  setAppSetting: async (data: { setting: Setting; value: string | number | boolean }) => IPCR.invoke('set-app-setting', data),
   getAllAppSettings: async () => IPCR.invoke('get-all-app-settings')
 }
 
