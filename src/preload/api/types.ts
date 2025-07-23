fde6a7bd930a0c4826b068b6ccb06d81af37a9d9
Louis <lourou99@gmail.com>
2025-07-23 22:49:11 +0200

    Added types

--- src/preload/api/types.ts
+++ src/preload/api/types.ts
@@ -28,7 +28,7 @@ type TestAPI = {
 type DatabaseAppSettingsAPI = {
   test: (v: { v: boolean }) => void
   getAppSetting: (data: { setting: Setting }) => Promise<any>
-  setAppSetting: (data: { setting: Setting; value: string }) => Promise<boolean>
+  setAppSetting: (data: { setting: Setting; value: string | number | boolean }) => Promise<boolean>
   getAllAppSettings: () => Promise<Record<string, string>[]> // TODO; Type
 }
 
