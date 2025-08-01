6eb84ca2d4ce0564bc829093c04c3a5a1a7eb202
Louis <lourou99@gmail.com>
2025-08-01 17:41:43 +0200

    Added resize listener component

--- src/renderer/src/libs/middlewear/index.tsx
+++ src/renderer/src/libs/middlewear/index.tsx
@@ -1,11 +1,13 @@
-import { DotSquadListener } from './dot-squad-listener'
 import { RouterListener } from './router-listener'
+import { DotSquadListener } from './dot-squad-listener'
+import { WindowResizeListener } from './resize-listener'
 
 export function Middlewear() {
   return (
     <>
       <RouterListener />
       <DotSquadListener />
+      <WindowResizeListener />
     </>
   )
 }
