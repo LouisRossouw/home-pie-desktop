2d4b403ea87a6a2153f130371a6889df1cb154eb
Louis <lourou99@gmail.com>
2025-08-04 17:57:01 +0200

    More types

--- src/shared/types.ts
+++ src/shared/types.ts
@@ -15,11 +15,26 @@ export type ApiProjectsList = { ok: boolean; data?: Project[] }
 
 export type Setting = (typeof defaultAppSettings)[number]['key']
 
-export type ResizeApp = { width: number; height: number }
+export type ResizeApp = {
+  width: number
+  height: number
+  save?: boolean
+}
 
-export type WindowBaseActions = 'minimize' | 'maximize' | 'close'
+export type WindowBaseActions = 'minimize' | 'maximize' | 'close' | 'login'
 export type WindowModes = 'sidebar-left' | 'bottom-left'
 
 export type WindowControl = {
   action: WindowBaseActions | WindowModes
+  width?: number
+  height?: number
+}
+
+export type OnResize = {
+  x: number
+  y: number
+  width: number
+  height: number
+  hasMoved?: boolean
+  isMoving?: boolean
 }
