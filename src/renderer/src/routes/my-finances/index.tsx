e4c6d630c413540d755ad98c22b3cd129d735478
Louis <lourou99@gmail.com>
2025-08-10 13:35:42 +0200

    Restructure to my finances route

---
+++ src/renderer/src/routes/my-finances/index.tsx
@@ -0,0 +1,40 @@
+// import { useMemo } from 'react'
+import { Outlet } from 'react-router'
+
+// import { Lightbulb } from 'lucide-react'
+// import { useQuery } from '@tanstack/react-query'
+
+// import { Button } from '~/components/ui/button'
+// import { useNav } from '~/libs/hooks/use-navigation'
+
+// import tempLogo from '../assets/projects/logo/timeinprogress-logo-play.gif'
+
+// const myFinancesAppRoutes = [
+//   {
+//     slug: 'something-something',
+//     img: undefined,
+//     url: '/my-fincances/something'
+//   }
+// ]
+
+export default function MyFinancesRoute() {
+  // const { navigateTo } = useNav()
+  // const { data: projectsRaw, isPending } = useQuery({
+  //   queryKey: ['projects-list'],
+  //   queryFn: getProjects
+  // })
+
+  // const myFinances = useMemo(() => {
+  //   return myFinancesAppRoutes?.map((proj) => {
+  //     const foundMyFinances = projectsRaw?.find((p) => p.slug === proj.slug)
+
+  //     return { ...proj, ...foundMyFinances, img: proj?.img ? proj.img : foundMyFinances?.img }
+  //   })
+  // }, [projectsRaw])
+
+  return (
+    <div className="flex h-[calc(100vh-96px)] items-center justify-center">
+      <Outlet />
+    </div>
+  )
+}
