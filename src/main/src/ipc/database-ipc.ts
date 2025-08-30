1d7224a2c8bd07b1de7688cbc4226e18f646c11a
Louis <lourou99@gmail.com>
2025-08-30 10:44:31 +0200

    Added core / user settings and session ipcs

--- src/main/src/ipc/database-ipc.ts
+++ src/main/src/ipc/database-ipc.ts
@@ -1,28 +1,41 @@
 import { ipcMain } from 'electron'
 
-import { getAllSettings, getSetting, setSetting } from '@main/src/db/settings'
-import { getAuth, setAuth } from '@main/src/db/auth'
 import { IpcKey } from '@shared/constants'
+import { getAllSessions, getSession, setSession } from '@main/src/db/session'
+import { getAllUserSettings, getUserSetting, setUserSetting } from '@main/src/db/user-settings'
+import { getAllCoreSettings, getCoreSetting, setCoreSetting } from '@main/src/db/core-settings'
 
 export function databaseIpcHandlers() {
   // * Settings
-  ipcMain.handle(IpcKey.getSetting, async (_event, data) => {
-    return await getSetting(data)
+  ipcMain.handle(IpcKey.getCoreSetting, async (_event, data) => {
+    return await getCoreSetting(data)
+  })
+  ipcMain.handle(IpcKey.setCoreSetting, async (_event, data) => {
+    return await setCoreSetting(data)
+  })
+  ipcMain.handle(IpcKey.getAllCoreSettings, async (_event) => {
+    return await getAllCoreSettings()
   })
 
-  ipcMain.handle(IpcKey.setSetting, async (_event, data) => {
-    return await setSetting(data)
+  // ** UserSettings
+  ipcMain.handle(IpcKey.getUserSetting, async (_event, data) => {
+    return await getUserSetting(data)
+  })
+  ipcMain.handle(IpcKey.setUserSetting, async (_event, data) => {
+    return await setUserSetting(data)
+  })
+  ipcMain.handle(IpcKey.getAllUserSettings, async (_event, data) => {
+    return await getAllUserSettings(data)
   })
 
-  ipcMain.handle(IpcKey.getAllSettings, async (_event) => {
-    return await getAllSettings()
+  // ** Session
+  ipcMain.handle(IpcKey.getSession, async (_event, data) => {
+    return await getSession(data)
   })
-
-  // ** Auth
-  ipcMain.handle(IpcKey.getAuth, async (_event, data) => {
-    return await getAuth(data)
+  ipcMain.handle(IpcKey.setSession, async (_event, data) => {
+    return await setSession(data)
   })
-  ipcMain.handle(IpcKey.setAuth, async (_event, data) => {
-    return await setAuth(data)
+  ipcMain.handle(IpcKey.getAllSessions, async (_event, data) => {
+    return await getAllSessions(data)
   })
 }
