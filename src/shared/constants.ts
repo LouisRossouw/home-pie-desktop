dc8a6b7a2981d520607cde35da53e2c8a1fd7c0a
Louis <lourou99@gmail.com>
2025-09-02 23:39:03 +0200

    More constants

--- src/shared/constants.ts
+++ src/shared/constants.ts
@@ -1,4 +1,14 @@
+import { version } from '../../package.json'
+import { getBaseURL } from './api'
+
 export const defaultDotSquadColour = 'rgb(20, 20, 20)'
+
+// const ENV = 'env'
+// const env = process[ENV]
+
+// const isDev = import.meta.env.DEV
+// const isProd = import.meta.env.PROD
+const appEnvironment = import.meta.env.MODE // development || production
 
 export enum IpcKey {
   // ** Core Settings
@@ -14,5 +24,29 @@ export enum IpcKey {
   // ** Session
   getSession = 'get-session',
   setSession = 'set-session',
-  getAllSessions = 'get-all-sessions'
+  getAllSessions = 'get-all-sessions',
+  checkAccessToken = 'check-access-token',
+
+  // ** Auth
+  signIn = 'sign-in'
 }
+
+export enum SessionKey {
+  accessToken = 'accessToken'
+}
+export const getAppVersion = () => version
+export const getAppPlatform = () => process.platform
+export const isMac = () => getAppPlatform() === 'darwin'
+export const isLinux = () => getAppPlatform() === 'linux'
+export const isWindows = () => getAppPlatform() === 'win32'
+export const isDevelopment = () => appEnvironment === 'development'
+// export const getAppBuildDate = () => new Date(import.meta.env.VITE_BUILD_DATE ?? '').toLocaleDateString(); // prettier-ignore
+
+export const DEBOUNCE_MILLIS = 100
+
+// Oauth redirect URL
+export const getAppName = 'HomePie'
+export const getOauthRedirectUrl = `${getBaseURL()}/oauth/redirect`; // prettier-ignore
+export const getAppWebsiteBaseURL = getBaseURL()
+
+export const defaultProtocol = `homepie${isDevelopment() ? '-dev' : ''}`
