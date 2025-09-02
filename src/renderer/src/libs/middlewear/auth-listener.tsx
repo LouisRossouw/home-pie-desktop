3741c3c911781b59fbaf03d54d5e39fdc69db48b
Louis <lourou99@gmail.com>
2025-09-02 23:37:23 +0200

    Auth listener

---
+++ src/renderer/src/libs/middlewear/auth-listener.tsx
@@ -0,0 +1,31 @@
+import { useEffect } from 'react'
+import { IpcRendererEvent } from 'electron'
+import { useNav } from '~/libs/hooks/use-navigation'
+
+export function AuthListener() {
+  const { navigateTo } = useNav()
+
+  useEffect(() => {
+    const cleanup = setupAuthListener()
+    return cleanup
+  }, [])
+
+  function setupAuthListener() {
+    const handler = (_event: IpcRendererEvent, { code }: { code: string }) => {
+      if (code) {
+        console.log('Received auth code:', code)
+        // navigateTo(`/auth/callback?code=${code}`)
+        navigateTo(`/`)
+      }
+    }
+
+    window.api.app.onAuthCode(handler)
+    console.log('AuthListener mounted.')
+
+    return () => {
+      window.api.app.removeListener(handler, 'auth-code')
+    }
+  }
+
+  return null
+}
