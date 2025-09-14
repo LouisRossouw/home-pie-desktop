cb442e7f121ac897295d9725e2f228be894ccd58
Louis <lourou99@gmail.com>
2025-09-14 11:20:12 +0200

    Switched to open-api

--- src/main/src/api/mr-ping-ping/api-app-status.ts
+++ src/main/src/api/mr-ping-ping/api-app-status.ts
@@ -1,23 +1,24 @@
 import { updateDotSquadActivity } from '@main/src/app'
-import { handleError, requireSession } from '@main/src/session'
+import { requireSession } from '@main/src/session'
 
 // TODO; Rename
 export async function apiMrPingPingAppStatus({ appName }: { appName: string }) {
   const apiClient = await requireSession()
 
   try {
-    const response = await apiClient.get(`/api/mr-ping-ping/apps/status/${appName}`)
+    const { response, data } = await apiClient.GET(`/api/mr-ping-ping/apps/status/{app_name}`, {
+      params: { path: { app_name: appName } }
+    })
 
     if (response.status === 200) {
       updateDotSquadActivity({ activity: 'simpleCheck' })
-      return response.data
+      return data
     }
 
     console.error('Something went wrong')
 
     return { ok: false }
   } catch (error) {
-    handleError(error)
     return undefined
   }
 }
