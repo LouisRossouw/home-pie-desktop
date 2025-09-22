a8567f46e1dcb83915086ae17a76d8cf522469a6
Louis <lourou99@gmail.com>
2025-09-22 11:24:44 +0200

    Other changes

--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -3,13 +3,19 @@ import './assets/themes'
 
 import ReactDOM from 'react-dom/client'
 import { HashRouter } from 'react-router'
+import OverlayContextProvider from './libs/context/overlay'
+import { LoadingOverlay } from './components/loading-overlay'
+
 import App from './App'
 // import React from 'react'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   // <React.StrictMode>
   <HashRouter>
-    <App />
+    <OverlayContextProvider>
+      <LoadingOverlay />
+      <App />
+    </OverlayContextProvider>
   </HashRouter>
   // </React.StrictMode>
 )
