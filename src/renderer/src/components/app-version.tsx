6a0142300f2fe1789df2fefaebb1cbb30bc5a0eb
Louis <lourou99@gmail.com>
2025-11-09 11:40:32 +0200

    Optional animatedWords added to app version

--- src/renderer/src/components/app-version.tsx
+++ src/renderer/src/components/app-version.tsx
@@ -1,11 +1,32 @@
 import { getAppVersion } from '@shared/constants'
 import { cn } from '~/libs/utils/cn'
+import { AnimatedWords } from './animated-words'
 
 const version = getAppVersion()
 
-export function AppVersion({ className }: { className?: string }) {
+export function AppVersion({
+  animateWords,
+  animateWordsOptions = { speed: 70, trailCount: 20, delay: 0 },
+  className
+}: {
+  animateWords?: boolean
+  animateWordsOptions?: { speed: number; trailCount: number; delay: number }
+  className?: string
+}) {
   return (
-    // {/* VERSION SEMANTICS; V - MAJOR - MINOR - PATCH */}
-    <p className={cn('text-light text-xs', className)}>v{version} (Alpha)</p>
+    <>
+      {animateWords ? (
+        <AnimatedWords
+          speed={animateWordsOptions.speed}
+          trailCount={animateWordsOptions.trailCount}
+          delay={animateWordsOptions.delay}
+          text={`${version} (Alpha)`}
+          className={className}
+        />
+      ) : (
+        // {/* VERSION SEMANTICS; V - MAJOR - MINOR - PATCH */}
+        <p className={cn('text-light text-xs', className)}>v{version} (Alpha)</p>
+      )}
+    </>
   )
 }
